ddc6a2165287222680a72827aba6d7e0561ee643
Dev <Dev@DESKTOP-A9ROM7F>
2023-07-06 12:12:44 -0700

    Add project files.

---
+++ RevolvingGamesGRPCClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using Grpc.Net.Client;
+using RevolvingGamesGRPCClient;
+using System.Numerics;
+
+var channel = GrpcChannel.ForAddress("http://localhost:5236");
+var client = new PrimeNumber.PrimeNumberClient(channel);
+
+Random rnd = new();
+
+Int64 count = 0;
+
+while (true)
+{
+    int primeNumber = rnd.Next(1, 1001);
+
+    var primeNumberData = new PrimeNumberModel
+    {
+        Id = count,
+        Timestamp = 2,
+        Number = primeNumber
+    };
+    count++;
+    var serverResponse = await client.IsPrimeNumberAsync(primeNumberData);
+    Console.WriteLine($"Is {primeNumber} a Prime Number: {serverResponse.IsPrime_}");
+}
---
+++ RevolvingGamesGRPCService/Services/PrimeService.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Core;
+using RevolvingGamesGRPCService.PrimeNumberProto;
+
+namespace RevolvingGamesGRPCService.Services
+{
+    public class PrimeService : PrimeNumber.PrimeNumberBase
+    {
+        private readonly ILogger<PrimeService> _logger;
+
+        public PrimeService(ILogger<PrimeService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<IsPrime> IsPrimeNumber(PrimeNumberModel request, ServerCallContext context)
+        {
+            if(request.Number < 2)
+            {
+                return Task.FromResult(new IsPrime { IsPrime_ = false }); 
+            }
+            for(int  i = 2; i <= Math.Sqrt(request.Number); i++)
+            {
+                if(request.Number % i == 0)
+                {
+                    return Task.FromResult(new IsPrime { IsPrime_ = false });
+                }
+            }
+            return Task.FromResult(new IsPrime { IsPrime_ = true });
+        }
+    }
+}
