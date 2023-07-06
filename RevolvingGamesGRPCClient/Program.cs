e2c0d51002ec9ef6210058ce7d742ac30c1eac54
dev-shoaib33 <devshoaib33@gmail.com>
2023-07-06 13:09:34 -0700

    Calculate and Print RTT for each request using StopWatch

--- RevolvingGamesGRPCClient/Program.cs
+++ RevolvingGamesGRPCClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Grpc.Net.Client;
 using RevolvingGamesGRPCClient;
+using System.Diagnostics;
 using System.Numerics;
 
 var channel = GrpcChannel.ForAddress("http://localhost:5236");
@@ -8,18 +9,29 @@ var client = new PrimeNumber.PrimeNumberClient(channel);
 Random rnd = new();
 
 Int64 count = 0;
+var stopwatch = new Stopwatch();
 
 while (true)
 {
+    stopwatch.Restart();
     int primeNumber = rnd.Next(1, 1001);
 
     var primeNumberData = new PrimeNumberModel
     {
         Id = count,
-        Timestamp = 2,
+        Timestamp = ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds(),
         Number = primeNumber
     };
     count++;
     var serverResponse = await client.IsPrimeNumberAsync(primeNumberData);
-    Console.WriteLine($"Is {primeNumber} a Prime Number: {serverResponse.IsPrime_}");
+    stopwatch.Stop();
+    if(serverResponse != null)
+    {
+        Console.WriteLine($"Is {primeNumber} a Prime Number: {serverResponse.IsPrime_}");
+        Console.WriteLine($"Time Taken: {stopwatch.ElapsedMilliseconds}");
+    }
+    else
+    {
+        Console.WriteLine("Response Missing!");
+    }
 }
